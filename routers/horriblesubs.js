956eded5e7099558368af1b27f4dbdf1e850a41e
Moshe Sherman <moshesher1998@gmail.com>
2019-11-08 20:11:24 +0200

    add documentation

--- routers/horriblesubs.js
+++ routers/horriblesubs.js
@@ -4,8 +4,13 @@ import Webtorrent from "webtorrent";
 import parseTorrent from "parse-torrent";
 import sendSeekable from "send-seekable";
 
+const app = express();
+app.use(sendSeekable);
+
+// once the server starts, a new  webtorrent client will start.
 let client = new Webtorrent();
 
+// search for a specific show, will return all horriblesubs files uploaded to nyaa.si
 export const getSources = async (term) => {
 	return await si.searchAllByUser({
 		user: "HorribleSubs",
@@ -14,9 +19,10 @@ export const getSources = async (term) => {
 	});
 };
 
-const app = express();
-app.use(sendSeekable);
-
+// GET localhost:port/horriblesubs/api/sources/one%20punch%20man?resolution=720p ->
+// will return all of "one punch man" sources with 720p resolution.
+// available resolutions: 480p, 720p, 1080p.
+// if no resolution specified, it will return all resolutions.
 app.get("/api/sources/:slug", (req, res) => {
 	getSources(req.params.slug).then(data => {
 		const hashedSources = data.map(file => (
@@ -33,20 +39,46 @@ app.get("/api/sources/:slug", (req, res) => {
 	});
 });
 
+// GET localhost:port/horriblesubs/stream/HASH
+// the request must come from a <video> tag with the url for the stream to work.
 app.get("/stream/:hash", (req, res) => {
+	// create a stream file in the request scope for later manipulation.
 	let stream;
 
+	// let's add the new hash to our client.
+	client.add(req.params.hash, torrent => {
+		console.log(torrent.name);
+		const file = torrent.files[0];
+		// create a streamable buffer and stream it over the connection.
+		// all of the headers are being manipulated by the `sendSeekable` middleware.
+		stream = file.createReadStream();
+		res.sendSeekable(stream, {length: file.length});
+	});
+
+	// this handles all of the errors, but we use it for one main error:
+	// the client already has the torrent hash downloading...
 	client.on("error", () => {
+		// we find this file with some filtering.
 		const fileExists = client.torrents.filter(({infoHash}) => req.params.hash === infoHash);
 		const file = fileExists[0].files[0];
 
+		// if the file doesnt exist, this means the error we encountered isn't the main one.
+		// TODO: log the unique error.
 		if (!file) {
+			// for now, returning is good enough.
 			return;
 		}
+		// what if we dont have a stream? this means the torrent client is already handling the file.
 		if (!stream) {
+			// we create a new stream buffer for piping.
 			stream = file.createReadStream();
 		}
 
+		// option A: the server already started streaming the file to this specific request
+		// option B: it doesn't.
+
+		// if it's options B, the try succeeds and we sendSeekable the stream with all the headers.
+		// if it's option A, the try failes and we pipe the stream without all of the headers.
 		try {
 			res.sendSeekable(stream, {length: file.length});
 		} catch {
@@ -54,13 +86,7 @@ app.get("/stream/:hash", (req, res) => {
 		}
 	});
 
-	client.add(req.params.hash, torrent => {
-		console.log(torrent.name);
-		const file = torrent.files[0];
-		stream = file.createReadStream();
-		res.sendSeekable(stream, {length: file.length});
-	});
-
+	// once the request has finished, we just log it.
 	req.on("close", () => {
 		console.log("closed");
 	});
--- routers/twist.js
+++ routers/twist.js
@@ -4,6 +4,8 @@ import express from "express";
 
 const baseURL = "https://twist.moe";
 const key = "LXgIVP&PorO68Rq7dTx8N^lP!Fa5sGJ^*XK";
+
+// I just the chrome browser user-agent
 const headers = {
 	"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36",
 	"referer": baseURL,
@@ -12,16 +14,21 @@ const headers = {
 
 export const API = Axios.create({baseURL, headers});
 
+// gets all the sources based on the slug. a slug is a title from kitsu.
+// TODO: standardize the slug here and in the horriblesubs router.
 export const getSources = async (slug) => {
 	const sources = await API.get(`/api/anime/${slug}/sources`, {
 		headers: {"x-access-token": "1rj2vRtegS8Y60B3w3qNZm5T2Q0TN2NR"},
 	});
+	// we recieve the sources with encryption. let's decrypt it!
 	return sources.data.map(data => {
 		const bytes = CryptoJS.AES.decrypt(data.source, key);
 		return {...data, "source": bytes.toString(CryptoJS.enc.Utf8).trim()};
 	});
 };
 
+// just a proxy from the twist.moe nginx server.
+// we're fooling it to think it's the official website requesting this data.
 export const getEpisodeStream = async (url, range) => {
 	const headers = range ? {"Range": range} : {};
 	return await API.get(url, {responseType: "stream",
@@ -31,16 +38,22 @@ export const getEpisodeStream = async (url, range) => {
 
 const app = express();
 
+// because the twist.moe nginx server returns headers that won't allow us to cross origin the data,
+// let's create a middleware that changes those tokens to something more friendly.
+// this is a possible security risk, but I couldn't care less.
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
 
+// GET localhost:port/twist/api/sources/one-punch-man -> returns all of the sources for one punch man.
 app.get("/api/sources/:slug", ({params: {slug}}, res) => {
 	getSources(slug).then(data => res.send(data));
 });
 
+// GET localhost:port/twist/anime/onepunchman/something-0001.mp4 (or something like that)
+// this is what the <video> tag url should be.
 app.get("/anime/:title/:episode", ({params, headers: {range}}, res, next) => {
 	const {title, episode} = params;
 	const url = `/anime/${title}/${episode}`;
